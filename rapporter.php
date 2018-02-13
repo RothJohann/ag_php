e07d98b2544a58de09082a88195debf8184da4d2
Johann Roth <johann.roth@gmail.com>
2018-02-13 23:00:21 +0100

    insertion du rapport de givre dans la Db meteo

--- rapporter.php
+++ rapporter.php
@@ -1,48 +1,26 @@
 <?php
+
+ try
+ {
+	 $bdd = new PDO('mysql:host=localhost;dbname=meteo;charset=utf8', 'root', '');
+ }
+ catch(Exception $e)
+ {
+		 die('Erreur : '.$e->getMessage());
+ }
  
-    //Récupération des données  
+ // On ajoute une entrée dans la table jeux_video
+ $bdd->exec('INSERT INTO rapportsgivre (DateRapport, Givre) VALUES (CURRENT_TIMESTAMP, 1)');
+ 
+
     $tab = array();
 	
-	$date = date("d/m/Y");
-		
-	if(substr($date,0,1)=="0")
 	
-	$date = substr($date,1,5) . substr($date,8,2);
+	//
 	
-	else
-	
-	$date = substr($date,0,5) . substr($date,8,2);
-	
-	
-	$heure = date("H:i");
-	//Print("Nous sommes le $date et il est $heure");
-	
-	$fichiermeteo = fopen('C:\WeatherLink\ADRETS\Downld08.txt', 'r');
-	$ligne = fgets($fichiermeteo);
-	do  //recherche de la 1ère ligne avec la date du jour
-	{
-	$ligne = fgets($fichiermeteo);
-	//print (substr($ligne,0,10) . " " . substr($ligne,11,1));
-	//print '<br>';
-	}	
-	while (trim(substr($ligne,0,10)) != $date and $ligne != null);
-	
-	//print ("on a trouvé la bonne date");
-	
-	do //recherche de la 1ère ligne à partir de 5h 
-	{
-	$ligne = fgets($fichiermeteo);
-	//print (substr($ligne,0,10) . " " . substr($ligne,11,1));
-	//print '<br>';
-	}
-	while (substr($ligne,11,1) != "5" and $ligne != null);	
-	
-	//print ("la temperature du $date à " . substr($ligne,11,5) . " est " . substr($ligne,18,7)."°C"); 
-	
-    $tab[0] = array("temp5h" => substr($ligne,18,4),"hum6h" => substr($ligne,40,2));
+    $tab[0] = array("msg" => 'le rapport a bien été inséré dans la base de données');
 
     print(json_encode($tab));
-	
-	fclose($fichiermeteo);
+
     
 ?>
