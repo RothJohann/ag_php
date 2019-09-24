5f408d88b1754c9f4d36516d8a1117b120bc01d6
Johann Roth <johann.roth@rte.fr>
2019-09-24 20:18:17 +0200

    rajout des accents dans le php

--- rapporter.php
+++ rapporter.php
@@ -19,7 +19,7 @@ header( 'content-type: text/html; charset=UTF-8' );
 	
 	
 	
-    $tab[0] = array("msg" => 'Le rapport a bien été inseré dans la base de données!');
+    $tab[0] = array("msg" => utf8_encode('Le rapport a bien été inseré dans la base de données!');
 
     print(json_encode($tab));
 
