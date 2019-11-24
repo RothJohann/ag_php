c208438bc3a219b623fa6ccee32f79ef16a15464
Johann Roth <johann.roth@rte.fr>
2019-11-24 17:59:38 +0100

    soutraction des 5 minutes de délai

--- rapporter.php
+++ rapporter.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set('Europe/Paris');
 
-$interval = new DateInterval('PT5i');
+$interval = new DateInterval('PT5M');
 $date = new DateTime();
 date_sub($date, $interval);
 $date = date_format($date, 'd/m/Y');
